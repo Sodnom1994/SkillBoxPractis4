a8537d671a950f860cd91c3caa1ff74638208d02
Sodnom1994 <Sodnom94@gmail.com>
2024-10-08 10:15:13 +0500

    +

--- Assets/Scripts/QuessScript.cs
+++ Assets/Scripts/QuessScript.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuessScript : MonoBehaviour
 {
-    
+    private int _number;
+    private Text _quessingnumber;
+
 }
